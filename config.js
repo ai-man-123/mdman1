15b1b01123763cc3a51d76fd8d76a946b420fcda
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-14 13:10:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6289505165400'] // Put your number here
+global.owner = ['60102810046'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -16,7 +16,7 @@ global.APIs = { // API Prefix
   LeysCoder: 'https://leyscoders-api.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
+  // 'https://website': 'https://s.id/yoaiman'
   'https://api.xteam.xyz': 'apivproject',
   'https://melcanz.com': 'elaina',
   'https://api.lolhuman.xyz': 'pkebgk8248jskrkfm',
@@ -28,11 +28,11 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'R-Txzy Multi device'
-global.author = 'AlyaaXzy'
-global.email = 'alyasaja@gmail.com'
+global.packname = 'aiman Multi device'
+global.author = 'Aiman'
+global.email = 'aimanmk1234@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© R-Txzy Md Made by AlyaaXzy'
+global.wm = '© Md Made by Aiman'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
